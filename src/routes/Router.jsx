75886d180a29f99fc7d38866416980a1d2511eae
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-22 23:19:46 +0600

    Initial project setup complete

---
+++ src/routes/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayOut from '../lay-outs/main-layout/MainLayOut';
+import Home from '../pages/home-page/Home';
+
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayOut />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default Router;
