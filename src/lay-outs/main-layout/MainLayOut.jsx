d34708b60aefde9ae60cb3ebc54f6326d18b77da
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-22 23:54:45 +0600

    Footer section created with resposive for all devices

--- src/lay-outs/main-layout/MainLayOut.jsx
+++ src/lay-outs/main-layout/MainLayOut.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../../pages/home-page/navbar/NavBar';
+import Footer from '../../pages/home-page/footer/Footer';
 
 const MainLayOut = () => {
   return (
     <div className="mx-3">
       <NavBar></NavBar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
