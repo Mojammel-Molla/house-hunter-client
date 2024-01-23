9c48327a1f1b18bf5941ce0e4c48b35a3fffde30
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-23 11:22:39 +0600

    Register page created

---
+++ src/components/register/Register.jsx
@@ -0,0 +1,70 @@
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+  const handleRegister = e => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, email, password);
+  };
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content w-full">
+        <div className="card shadow-2xl w-1/3 mx-auto bg-base-100">
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                name="name"
+                type="text"
+                placeholder="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <p>
+                Don't have account please{' '}
+                <Link to="/login" className="text-blue-600 text-sm">
+                  Register
+                </Link>
+              </p>
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
